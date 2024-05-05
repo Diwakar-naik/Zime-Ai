32877ef60adaa7aaab5a69c2a467165955778b3e
Diwakar-naik <divakarnaik716@gmail.com>
2024-05-05 16:21:43 +0530

    first commit

---
+++ pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TableComponent from '../components/TableComponent'
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Hello My name is Diwakar naik. This is my assignment</h1>
+        <TableComponent/>
+
+    </div>
+  )
+}
